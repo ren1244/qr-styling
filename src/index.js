f53f3fd9dd3c52ef700c39297e656dba34fff30b
ren1244 <n1244506804@hotmail.com>
2025-08-25 16:45:37 +0800

    Using core/qrcode.js as default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+import QrCore from './core/qrcode.js';
 import QrBase from "./qr-base.js";
 import StylingBase from "./styling/styling-base.js";
 
-export { QrBase, StylingBase };
+class QrCode extends QrBase {
+    static create(data, errorCorrectionLevel, version) {
+        const qr = new QrCore(data, errorCorrectionLevel, version);
+        const size = qr.getSize();
+        const arr = new Uint8Array(size * size);
+        for (let r = 0; r < size; ++r) {
+            for (let c = 0; c < size; ++c) {
+                arr[r * size + c] = qr.getPoint(r, c);
+            }
+        }
+        return { size, data: arr };
+    }
+}
+
+export { QrCode, QrBase, StylingBase };
